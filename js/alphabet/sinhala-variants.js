2b138a9ca3d0e2e07fb408ba126eadeb9b3ecc14
HelaNomad Tech <helanomad.tech@gmail.com>
2025-12-19 17:36:20 +0530

    update sinhala variant texts

--- js/alphabet/sinhala-variants.js
+++ js/alphabet/sinhala-variants.js
@@ -2,8 +2,23 @@ export const SINHALA_VARIANTS = {
   // null means "show everything"
   nie: null,
 
-  // Amishra: core Sinhala set (exclude the obvious Sanskrit/Pali-heavy extras)
-  amishra: new Set([
+  // Miśra: expanded set = Amishra + aspirates + ś/ṣ + Sanskritic vowels
+  miśra: new Set([
+    ...[
+      "a","ā","ä","æ","ǟ","ǣ","i","ī","u","ū","e","ē","ai","o","ō","au",
+      "k","g","ṅ","c","j","ñ","ṭ","ḍ","ṇ","t","d","n","p","b","m",
+      "y","r","l","v","s","h","ḷ",
+      "ṁ","ṃ","ḥ"
+    ],
+    // extras present on the Sinhala learn page
+    "kh","gh","ch","jh","ṭh","ḍh","th","dh","ph","bh",
+    "ś","ṣ",
+    // Sanskritic vowels that Sinhala supports in Unicode set
+    "ṛ","r̥","ṝ","r̥̄","l̥","l̥̄"
+  ]),
+  
+  // Amiśra: core Sinhala set (exclude Sanskrit/Pali-heavy extras)
+  amiśra: new Set([
     // Vowels (include Sinhala-specific æ/ä & long forms)
     "a","ā","ä","æ","ǟ","ǣ","i","ī","u","ū","e","ē","o","ō",
 
@@ -19,23 +34,8 @@ export const SINHALA_VARIANTS = {
     "ṁ","ṃ"
   ]),
 
-  // Mishra: expanded set = Amishra + aspirates + ś/ṣ/f + Sanskritic vowels
-  mishra: new Set([
-    ...[
-      "a","ā","ä","æ","ǟ","ǣ","i","ī","u","ū","e","ē","ai","o","ō","au",
-      "k","g","ṅ","c","j","ñ","ṭ","ḍ","ṇ","t","d","n","p","b","m",
-      "y","r","l","v","s","h","ḷ",
-      "ṁ","ṃ","ḥ"
-    ],
-    // extras present on your Sinhala learn page
-    "kh","gh","ch","jh","ṭh","ḍh","th","dh","ph","bh",
-    "ś","ṣ",
-    // Sanskritic vowels that Sinhala supports in Unicode set
-    "ṛ","r̥","ṝ","r̥̄","l̥","l̥̄"
-  ]),
-
-  // Sidath: conservative classical baseline
-  sidath: new Set([
+  // Sidat: conservative classical baseline
+  sidat: new Set([
     "a","ā","i","ī","u","ū","e","ē","o","ō",
     "k","g","j","ṭ","ḍ","ṇ","t","d","n","p","b","m",
     "y","r","l","v","s","h","ḷ",
